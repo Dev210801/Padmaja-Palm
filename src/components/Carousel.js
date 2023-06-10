360449f7c67a3b2b962033f811d2de8bbc609cd8
dhanush <dhanushg25@gmail.com>
2023-06-10 18:48:26 +0530

    carousel to be added

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,12 @@
-import React, {useState} from 'react'
+import React from 'react'
 import img1 from "../asserts/istockphoto-517188688-612x612.jpg"
-import img2 from "../asserts/logo3 with bg.png"
+// import img2 from "../asserts/logo3 with bg.png"
 
 export default function Carousel() {
-  const [selectedimg, setselectedimg] = useState(img1);
-  const [allimgs, setallimgs] = useState([img1, img2, img1, img2])
+ 
     return (
         <div>
-        <img src={selectedimg} alt="" />
+        <img src={img1} alt="" />
         <div style={{display:"flex"}}>
             
         </div>
