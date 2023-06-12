db4a5bf77d44ea075b9541ebf99382271dae9810
dhanush <dhanushg25@gmail.com>
2023-06-12 23:10:34 +0530

    map is left

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import '../styles/contact.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Mapsetup from "./Mapsetup";
 export default function Contact() {
   return (
     <div className="outter1"><br /><br />
@@ -15,6 +16,9 @@ export default function Contact() {
         {'\u00A0'} sy no 294, Airport road Mamidpally, Balapur mandal Rangareddy dist. <br />
 Landmark : Mamidpally to Jalpally Rd, Hyderabad, Telangana 500005.</h5>
       <h6>Nearby Rajiv Gandhi International Airport, And Nehru The Outer Ring Road is just a few minutes away.</h6>
+    <div className="mapset1">
+      <Mapsetup/>
+    </div>
     </div>
   );
 }
