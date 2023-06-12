8f1d25c29140baa2c8bbcbcb4fcd04593a572eee
dhanush <dhanushg25@gmail.com>
2023-06-12 22:31:44 +0530

    contact page done final check

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,20 @@
 import React from "react";
 import '../styles/contact.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 export default function Contact() {
   return (
-    <div className="outter1">
-      <p>Contact</p>
+    <div className="outter1"><br /><br />
+      <h1>Contact</h1>
+      
+      <h3><FontAwesomeIcon icon={faPhone}/> 090007 79444</h3>
+      <h3><FontAwesomeIcon icon={faEnvelope}/> padmajapalms@gmail.com</h3><br />
+      <h5> <span className="ss">
+         ADDRESS: 
+        </span>
+        {'\u00A0'} sy no 294, airport road mamidpally, balapur mandal rangareddy dist. <br />
+landmark : mamidpally to Jalpally Rd, Hyderabad, Telangana 500005.</h5>
+      <h6>Rajiv Gandhi International is very nearby, and Nehru The Outer Ring Road is just a few minutes away.</h6>
     </div>
   );
 }
