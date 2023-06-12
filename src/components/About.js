f49023d40ad7f82bff25c0da01b2853f22e009a1
dhanush <dhanushg25@gmail.com>
2023-06-12 20:57:24 +0530

    contraints fixed

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/About.css";
-import  Carousel  from "./Carousel.js";
+// import  Carousel  from "./Carousel.js";
 
 export default function About() {
   return (
