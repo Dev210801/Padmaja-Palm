8f7915bacc38eacb1597637178990f1f1b85f540
dhanush <dhanushg25@gmail.com>
2023-07-15 21:29:50 +0530

    accomo

--- src/jsons/venues_pics.js
+++ src/jsons/venues_pics.js
@@ -1,4 +1,4 @@
-import bank1 from "../asserts/resortpics/banquet hall/5.jpg"
+
 import bank2 from "../asserts/resortpics/banquet hall/6.jpg"
 import bank3 from "../asserts/resortpics/banquet hall/3.JPG"
 import lawn1 from "../asserts/resortpics/lawn 1/1.JPG"
@@ -7,6 +7,6 @@ import lawn4 from "../asserts/resortpics/lawn 1/3.JPG"
 import ph1 from "../asserts/resortpics/party hall/1.JPG"
 import ph2 from "../asserts/resortpics/party hall/2.JPG"
 import ph3 from "../asserts/resortpics/party hall/3.JPG"
-export const Banquet = [bank1, bank2, bank3];
+export const Banquet = [ bank2, bank3];
 export const lawn = [lawn1, lawn3, lawn4];
 export const ph = [ph1, ph2, ph3];
