96056b443ad2039e637df89b2aa1333a605c4ecb
dhanush <dhanushg25@gmail.com>
2023-07-15 21:35:24 +0530

    accomo

--- src/jsons/venues_pics.js
+++ src/jsons/venues_pics.js
@@ -1,4 +1,4 @@
-import bank1 from "../asserts/resortpics/banquet hall/5.jpg"
+import bank1 from "../asserts/resortpics/banquet hall/2.JPG"
 import bank2 from "../asserts/resortpics/banquet hall/6.jpg"
 import bank3 from "../asserts/resortpics/banquet hall/3.JPG"
 import lawn1 from "../asserts/resortpics/lawn 1/1.JPG"
